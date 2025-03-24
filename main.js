d79e481f52b22972879f69922434b0fc3fba5de6
Kenjie Tarasona-wsl <148972543+voidKing00@users.noreply.github.com>
2025-03-25 00:56:47 +0800

    Implement insert(value) in the tree

--- main.js
+++ main.js
@@ -32,24 +32,61 @@ class Tree {
   }
 
   buildTreeRecursiveHelper(array, start, end) {
+    if (start > end) return null;
 
-    if(start > end) return null;
-
-    const mid = Math.floor((end + start) / 2); 
+    const mid = Math.floor((end + start) / 2);
     const temp = createNode(array[mid]);
-
+    console.log(mid);
+    console.log(array);
+    print(temp.data);
     temp.left = this.buildTreeRecursiveHelper(array, start, mid - 1);
     temp.right = this.buildTreeRecursiveHelper(array, mid + 1, end);
 
     return temp;
-
   }
 
-  getRoot(){
+  getRoot() {
     return this.root;
   }
+
+  insert(value) {
+
+    const newNode = createNode(value);
+    let currentNode = this.root;
+
+    while (true) {
+      if (this.root === null) return null;
+
+      let smaller;
+
+      newNode.data < currentNode.data ? (smaller = true) : (smaller = false);
+
+      if (smaller) {
+        if (currentNode.left === null) {
+          currentNode.left = newNode;
+          return;
+        }
+
+        currentNode = currentNode.left;
+        continue;
+
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = newNode;
+          return;
+        }
+
+        currentNode = currentNode.right;
+        continue;
+      }
+    }
+  }
 }
+const numbers = [15, 10, 20, 8, 12, 18, 25, 6, 9, 11];
 
 const tree = new Tree();
-tree.buildTree([3,2,1]);
+
+tree.buildTree(numbers);
+prettyPrint(tree.getRoot());
+tree.insert(19);
 prettyPrint(tree.getRoot());
